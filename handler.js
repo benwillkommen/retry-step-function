cfce9ebad81f86ef3ad669b86734085e8139e604
ben willkommen <benwillkommen@gmail.com>
2018-10-08 20:50:43 -0500

    got serverless to create a step function with what appear to be a correct state machine definition. still need to make api gateway and each task/state work.

---
+++ handler.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.tryRequest = async(event, context) => {
+  return { 
+    message: 'did "tryRequest"',
+    requestSucceeded: true,
+    waitSeconds: 2,
+    event };
+};
